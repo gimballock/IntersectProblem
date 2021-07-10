c5f08667cfab538f29e65342ed0ad8e10385823f
Eric Price <priceeric1981@gmail.com>
2021-07-10 17:47:18 -0400

    Added more notes to README, change webpack config to use dev mode, initial support for AABB intersection test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {
     WebGLRenderer, Scene, PerspectiveCamera,
     ConeGeometry, TorusKnotGeometry, BoxBufferGeometry,
     MeshPhongMaterial, MeshNormalMaterial,
-    Vector3,
+    Vector3, Box3, Sphere,
     DirectionalLight,
     Mesh,
     BoxHelper, AxesHelper
@@ -12,8 +12,20 @@ import { DragControls } from "three/examples/jsm/controls/DragControls.js";
 import { WEBGL } from 'three/examples/jsm/WebGL.js';
 
 // Used to print status message every <LOG_INTERVAL> seconds 
-// by comparing current time to previous time whenever render() is called
-const LOG_INTERVAL = 5
+// by comparing current time to prevTime whenever render() is called
+const LOG_INTERVAL = 0.5
+
+// Error term to decide if objects are "kissing" vs inersecting or not-intersecting
+const ERROR_TERM = 0.1
+
+const IntersectState = {
+	AMBIGIOUS: "Ambigious",
+	NO_INTERSECT: "Not intersecting",
+	INTERSECT: "Intersecting"
+}
+
+const Dimension = { X:0, Y:1, Z:2 }
+
 let prevTime = 0
 
 let renderer, scene, camera;
@@ -80,7 +92,6 @@ function init() {
 
     const primaryGeom = new TorusKnotGeometry();
     primaryObj = makeInstance(primaryGeom, 0x44aa88, 0, 0);
-    //primaryObj.rotation.y = Math.PI * 0.25;
 
     const secondaryGeom = new ConeGeometry();
     secondaryObj = makeInstance(secondaryGeom, 0x8844aa, 0, 0)
@@ -102,8 +113,8 @@ function updateScene(time) {
     const rot = time * speed;
 
     // to show how the bounding shapes change over time
-    primaryObj.rotation.x = rot;
-    primaryObj.rotation.y = rot;
+    // primaryObj.rotation.x = rot;
+    // primaryObj.rotation.y = rot;
 
     secondaryObj.rotation.x = -rot;
     secondaryObj.rotation.y = -rot;
@@ -112,8 +123,16 @@ function updateScene(time) {
     // secondary object should move past the primary object centered at the origin 
     // - modulate the time by the length of the path you want it to traverse
     // - now subtract half of the total path length to center the path on the origin
-    let t = time % pathLength
-    secondaryObj.position.z = t - pathLength / 2 
+    let t = time % pathLength;
+    let target = t - pathLength / 2;
+
+    secondaryObj.position.z = target;
+
+    // update bounding boxes
+    helpers.forEach((helper) => { helper.update() })
+    // primaryObj.geometry.computeBoundingSphere();
+    // secondaryObj.geometry.computeBoundingSphere();
+
 }
 
 function render(time) {
@@ -124,51 +143,141 @@ function render(time) {
     
 
     // ------------------- PROCESS INTERSECTIONS HERE ----------------------
-    //OPTIONAL TODO: Add velocity vector to each object to bounce them off each other
 
-    // Types of shapes or approximate bounding volume: 
-    // - spheres, 
-    // - boxes, 
-    // - convex polyhedra, 
-    // - faceted closed non-self-intersecting
-
-    // Bounding Sphere: intersections are computed using r1 and r2 for the radius of circles 1 and 2 respectively.
-    // - ambigious:        circles are just touching of they are exactly r1 + r2 distance away from each other
-    // - not intersecting: circles are more than r1 + r2 distance away from each other
-    // - intersecting:     circles are less than r1 + r2 distance away from each other
-
-    // Bounding Boxe (axis-aligned): Box doesn't rotate as the bounded object changes, parameters (h, w) for each obj
-    // - ambigious:        boxes are just touching if they are exactly w1 + w2 distance apart horizontally 
-    //                     and/or h1 + h2 distance apart vertically
-    // - not intersecting: boxes are more than r1 + r2 distance away from each other
-    // - intersecting:     circles are less than r1 + r2 distance away from each other
     
-    // Convex-hulls: Compute bounding shape that ignores all concavity, 
-    // - e.g. a 5-sided start would become a pentagon, pacman w/ a face mask  
-    // - https://www.bowdoin.edu/~ltoma/teaching/cs3250-CompGeom/spring17/Lectures/cg-convexintersection.pdf
-
-    // Ray-casting: ?
-    //   - if bb (or bc) is not intersecting then return not intersecting
-    //   - find verticies from one of the objects within the region of intersection
-    //   -  cast a ray from each point and use ray-intersection test against the other object
-    //      - i guess you can bound the "length" of the ray so that it's not an infinite beam
-
-
     // looped status log
     if (time > (prevTime + LOG_INTERVAL)) {
-        console.log(Math.trunc(time), "Obj1", 
-            primaryObj.position.x, primaryObj.position.y, 
-            primaryObj.geometry.boundingBox, primaryObj.geometry.boundingSphere);
-        console.log(Math.trunc(time), "Obj2", 
-            secondaryObj.position.x, secondaryObj.position.y, 
-            secondaryObj.geometry.boundingBox, secondaryObj.geometry.boundingSphere);
+        
+        let sphereState = IntersectState.INTERSECT
+
+        let bs1 = (new Sphere())
+            .copy(primaryObj.geometry.boundingSphere)
+            .applyMatrix4( primaryObj.matrixWorld );
+        let bs2 = (new Sphere())
+            .copy(secondaryObj.geometry.boundingSphere)
+            .applyMatrix4( secondaryObj.matrixWorld );
+        
+        // TODO: use squared distance to avoid the division
+        let distance = bs1.center.distanceTo(bs2.center)
+        let radiusSum = bs1.radius + bs2.radius
+        let termDiff = distance - radiusSum 
+
+        if( Math.abs(termDiff) < ERROR_TERM )
+            sphereState = IntersectState.AMBIGIOUS;
+        else if( termDiff > 0)
+            sphereState = IntersectState.NO_INTERSECT;
+        // else sphereState = IntersectState.INTERSECT; <-- default value
+
+        console.log(Math.trunc(time), "Sphere", sphereState, termDiff);
+
+        // -------------------------------------------
+
+
+        // Seperately for vertical and horizontal: [distance between boxes] vs [width of the closer box]
+        // [distance between boxes]: Take the distance between the min points (or equivalently the max point from each box)
+        // [width of the closer box]: Get the width of either box 1 or box 2
+        //                            { bb1.min.x < bb2.min.x -> bb1.max.x - bb1.min.x
+        //                              otherwise             -> bb2.max.x - bb2.min.x 
+        // - ambigious:        if equal then the boxes are touching
+        // - non-intersecting: distance > width
+        // - intersecting:     distance < width
+
+        // To combine the vertical and horizontal intersections use the following table:
+        //                  vertical
+        // horizontal           | ambigious        | intersecting     | non-intersecting
+        //     -----------------+------------------+------------------+------------------
+        //     ambigious        | ambigious        | ambigious        | non-intersecting
+        //     -----------------+------------------+------------------+------------------
+        //     intersecting     | ambigious        | intersecting     | non-intersecting
+        //     -----------------+------------------+------------------+------------------
+        //     non-intersecting | non-intersecting | non-intersecting | non-intersecting
+        //
+        // In 3 dimensions its more complicated but we can see some patterns:
+        // - if any dimension is non-intersecting then the overall state is non-intersecting
+        // - otherwise if any dimension is ambigious then the overall state is ambigious
+        // - finally all dimensions must be intersecting so overall is intersecting
+
+
+        // Box3 objects are 3D cubes, they have 8 verticies but the THREE.js object stores:
+        // - .min (Vertex3): the point closest to the origin
+        // - .max (Vertex3): furtherest from the origin
+        // - .getSize() (Vertex3): the <width, height, depth> of the cube.
+        // Note: The other 6 points can be obtained by combining the x,y,z components of the min and max points
+
+        let boxStates = new Array(3)
+        
+        const bb1 = (new Box3())
+            .copy( primaryObj.geometry.boundingBox )
+            .applyMatrix4( primaryObj.matrixWorld );
+        const bb1Sizes = bb1.getSize(new Vector3())
+        
+        const bb2 = (new Box3())
+            .copy( secondaryObj.geometry.boundingBox )
+            .applyMatrix4( secondaryObj.matrixWorld );
+        const bb2Lengths = bb2.getSize(new Vector3())
+
+        for (const dimName in Dimension) {
+            const dimIdx = Dimension[dimName]; // get the index of the dimension: [x,y,z] -> [0,1,2]
+            let dimState = IntersectState.INTERSECT
+
+            // For the current dimension (x,y,z) set these values
+            const [bb1Min, bb2Min] = [bb1.min.getComponent(dimIdx), bb2.min.getComponent(dimIdx)]
+            const [bb1Length, bb2Length] = [bb1Sizes.getComponent(dimIdx), bb2Lengths.getComponent(dimIdx)]
+            
+            // 1) If boxes have same min point (could alternatively use max point of each box)
+            //    - if one of the widths is zero --> ambigious
+            //    - else                         --> intersecting
+            let isCoincident = (bb1Min == bb2Min)
+            let isDegenerate = (bb1Length <= ERROR_TERM) || (bb2Length <= ERROR_TERM)
+            if(isCoincident)
+                dimState = isDegenerate 
+                    ? IntersectState.AMBIGIOUS   // same start and one has no range --> just touching edge of other box
+                    : IntersectState.INTERSECT;  // same start and both have non-zero range --> intersecting
+
+            else {
+                // 2) Rename box1 and box2 as nearBox and farBox since we know they are not the same location
+                let [nearBoxMin, farBoxMin] = Math.abs(bb1Min) < Math.abs(bb2Min) ? [bb1Min, bb2Min] : [bb2Min, bb1Min]
+                    
+                // 3) Set distance between boxes and length of nearist box (to the origin)
+                let nearBoxLen = (nearBoxMin == bb1Min) ? bb1Length : bb2Length
+                let distance = Math.abs(farBoxMin - nearBoxMin) // equivalently to using max point
+                
+                // 4) Compare [distance between boxes] vs [width of the closer box]
+                let termDiff = distance - nearBoxLen
+                if( Math.abs(termDiff) < ERROR_TERM )
+                    dimState = IntersectState.AMBIGIOUS;
+                else if( termDiff > 0)
+                    dimState = IntersectState.NO_INTERSECT;
+                // else dimState = IntersectState.INTERSECT; <-- default value
+            }
+
+            boxStates[dimIdx] = dimState
+            // console.log(Math.trunc(time), "Bounding Box", dimIdx == 0 ? 'x' : dimIdx == 1 ? 'y' : 'z', dimState);
+        }
+
+        // Combine the dimensional box states to get the overall box state:
+        // - if any dimension is non-intersecting then the overall state is non-intersecting
+        // - otherwise if any dimension is ambigious then the overall state is ambigious
+        // - finally all dimensions must be intersecting so overall is intersecting
+        let overallBoxState = IntersectState.INTERSECT
+        for (const dimState of boxStates){
+            if(dimState == IntersectState.NO_INTERSECT) {
+                overallBoxState = IntersectState.NO_INTERSECT
+                break;
+            } 
+            const isAlreadyAmbigious = (overallBoxState == IntersectState.AMBIGIOUS)
+            if(!isAlreadyAmbigious && dimState == IntersectState.AMBIGIOUS)
+                overallBoxState = IntersectState.AMBIGIOUS;
+        }
+
+        console.log(Math.trunc(time), "Box", overallBoxState, boxStates);
+
         prevTime = time
     }
 
     // -------------------------------------------------------------------
 
-    helpers.forEach((helper) => { helper.update() })
-
+    
     // draw scene
     renderer.render(scene, camera);
 
